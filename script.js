0b3b65d1e892c4f65742020aa473c1a19ab46680
Michelle Lieng <michellelieng2020@gmail.com>
2025-04-19 16:40:21 +1000

    replaced single event listeners with event delegation

--- script.js
+++ script.js
@@ -3,26 +3,49 @@ let countEl = document.querySelector("#countMatchaValue");
 console.log(countEl.innerHTML);
 console.log(countMatcha);
 
-let addButton = document.querySelector("#add");
-let subtractButton = document.querySelector('#subtract');
-let resetButton = document.querySelector('#reset');
-// console.log(addButton.innerHTML) 
-
 // Event listener is you go add the action type and then the function when that happens
-addButton.addEventListener("click", increaseMatcha);
-function increaseMatcha () {
-    countMatcha += 1;
-    countEl.innerHTML = countMatcha;
-}
+let buttonContainer = document.querySelector('#buttons')
 
-subtractButton.addEventListener("click", decreaseMatcha)
-function decreaseMatcha () {
-    countMatcha -= 1;
-    countEl.innerHTML = countMatcha;
-}
+buttonContainer.addEventListener("click", function(event){
+    let buttonValue = event.target.textContent
+    console.log(buttonValue)
 
-resetButton.addEventListener("click", resetMatcha)
-function resetMatcha () {
-    countMatcha = 0;
-    countEl.innerHTML = countMatcha;
-}
+    if (buttonValue == "+") {
+        countMatcha += 1;
+        countEl.innerHTML = countMatcha;
+    }
+
+    else if (buttonValue == "-") {
+        countMatcha -= 1;
+        countEl.innerHTML = countMatcha;
+    }
+
+    else if (buttonValue == "Reset") {
+        countMatcha = 0;
+        countEl.innerHTML = countMatcha;
+    }
+})
+
+
+// let addButton = document.querySelector("#add");
+// let subtractButton = document.querySelector('#subtract');
+// let resetButton = document.querySelector('#reset');
+// // console.log(addButton.innerHTML) 
+
+// addButton.addEventListener("click", increaseMatcha);
+// function increaseMatcha () {
+//     countMatcha += 1;
+//     countEl.innerHTML = countMatcha;
+// }
+
+// subtractButton.addEventListener("click", decreaseMatcha)
+// function decreaseMatcha () {
+//     countMatcha -= 1;
+//     countEl.innerHTML = countMatcha;
+// }
+
+// resetButton.addEventListener("click", resetMatcha)
+// function resetMatcha () {
+//     countMatcha = 0;
+//     countEl.innerHTML = countMatcha;
+// }
